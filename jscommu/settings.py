589eb2704bb082587cfc4c39c563ed53ed0b06db
Yoon_JW <jiwookyoon0744@gmail.com>
2024-08-23 13:01:06 +0900

    change templates location

--- jscommu/settings.py
+++ jscommu/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jscommu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
